ad537520f464c4558ec42825a56ed73969a5eafe
Alex Chernyakevich <tch@scand.com>
2023-12-30 01:20:43 +0300

    Refactoring: *.sh made executable; added ./tmp folder; add-feed.sh - variable naming, optimizations, etc. (unfinished)

--- bin/add-feed.sh
+++ bin/add-feed.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
 PODCAST_ID=$1
-
-if [ "$#" -ne 1 ] || ! [[ "$PODCAST_ID" =~ ^[0-9]+$ ]]; then
-    echo "Error: wrong podcast id format"
-    exit 1
+if [ -z "$PODCAST_ID" ]; then
+    echo -e "Usage:\t./bin/add-feed.sh <podcast_id>"
+    echo -e "\nAdd empty RSS feed for smotrim.ru podcast with <podcast_id>."
+else
+    if ! [[ "$PODCAST_ID" =~ ^[0-9]+$ ]]; then
+        echo "Error: wrong podcast id format (it should be number)."
+        exit 1
+    fi
 fi
 
-BASE_DIR="../feeds"
-MAPPING_PATH="../bin/mapping.json"
-HEADER_FILE="$BASE_DIR/$PODCAST_ID/header_$PODCAST_ID.xml"
-RSS_FILE="$BASE_DIR/$PODCAST_ID/rss_$PODCAST_ID.xml"
 
-cd "$BASE_DIR" || exit
+MAPPING_PATH="./bin/mapping.json"
+FEEDS_BASE_DIR="./feeds"
+PODCAST_FEED_DIR="$FEEDS_BASE_DIR/$PODCAST_ID"
+HEADER_FILE_PATH="$PODCAST_FEED_DIR/header_$PODCAST_ID.xml"
+RSS_FILE_PATH="$PODCAST_FEED_DIR/rss_$PODCAST_ID.xml"
 
-if [ -d "$PODCAST_ID" ]; then
-    echo "Error: directory '$PODCAST_ID' already exists. Check podcast id or remove this directory"
+if [ -d "$PODCAST_FEED_DIR" ]; then
+    echo "Error: podcast's feed folder already exists ($PODCAST_FEED_DIR). Check podcast id or remove target RSS folder."
     exit 3
 fi
+mkdir "$PODCAST_FEED_DIR"
 
-mkdir "$PODCAST_ID"
 
-PODCAST_PATH="../tmp/podcast.html"
-RESPONSE=$(curl -s -w '%{http_code}' -o "$PODCAST_PATH" "https://smotrim.ru/podcast/$PODCAST_ID")
+PODCAST_HTML_PATH="./tmp/podcast-$PODCAST_ID.html"
+RESPONSE=$(curl -s -w '%{http_code}' -o "$PODCAST_HTML_PATH" "https://smotrim.ru/podcast/$PODCAST_ID")
 STATUS=$(echo $RESPONSE | awk '{print $NF}')
 
 if [ "$STATUS" -ne 200 ]; then
@@ -35,12 +39,12 @@ API_RESPONSE=$(curl -s -X 'POST' \
   'https://api.d2d.work/document/process' \
   -H 'accept: application/json' \
   -H 'Content-Type: multipart/form-data' \
-  -F "document=@$PODCAST_PATH" \
+  -F "document=@$PODCAST_HTML_PATH" \
   -F "metadata=@$MAPPING_PATH")
 
-rm "$PODCAST_PATH"
+rm "$PODCAST_HTML_PATH"
 
-cat <<EOF > "$HEADER_FILE"
+cat <<EOF > "$HEADER_FILE_PATH"
 <rss version="2.0">
     <channel>
         <title>$(echo "$API_RESPONSE" | jq -r '.result.title')</title>
@@ -54,4 +58,4 @@ cat <<EOF > "$HEADER_FILE"
 </rss>
 EOF
 
-cp "$HEADER_FILE" "$RSS_FILE"
+cp "$HEADER_FILE_PATH" "$RSS_FILE_PATH"
