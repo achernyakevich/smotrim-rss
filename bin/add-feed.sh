203ff87fda5391e67e0dd5970458e8ce66d9b4eb
Alex Chernyakevich <tch@scand.com>
2023-12-30 11:15:29 +0300

    Refactoring: structuring JSON as resulting RSS XML

--- bin/add-feed.sh
+++ bin/add-feed.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-clean_up() {
-    echo "Clean up begins:"
+clean_up_feed_dir() {
     rm -vrf "$PODCAST_FEED_DIR"
+}
+clean_up_html() {
     rm -v "$PODCAST_HTML_PATH"
-#    rm -rf "$PODCAST_FEED_DIR"
-#    rm -rf "$PODCAST_FEED_DIR"
-    echo "Clean up ends"
 }
 
 PODCAST_ID=$1
@@ -15,7 +13,7 @@ SCRIPT_FLAG=$2
 MAPPING_PATH="./bin/mapping.json"
 FEEDS_BASE_DIR="./feeds"
 PODCAST_FEED_DIR="$FEEDS_BASE_DIR/$PODCAST_ID"
-HEADER_FILE_PATH="$PODCAST_FEED_DIR/header_$PODCAST_ID.xml"
+HEADER_FILE_PATH="$PODCAST_FEED_DIR/rss_$PODCAST_ID.header"
 RSS_FILE_PATH="$PODCAST_FEED_DIR/rss_$PODCAST_ID.xml"
 PODCAST_HTML_PATH="./tmp/podcast-$PODCAST_ID.html"
 
@@ -41,7 +39,7 @@ STATUS=$(echo $RESPONSE | awk '{print $NF}')
 
 if [ "$STATUS" -ne 200 ]; then
     echo "Error: can't download podcast's HTML (URL: https://smotrim.ru/podcast/$PODCAST_ID, HTTP Status: $STATUS)"
-    clean_up
+    clean_up_feed_dir
     exit 1
 fi
 
@@ -52,20 +50,20 @@ PODCAST_JSON=$(curl -s -X 'POST' \
   -F "document=@$PODCAST_HTML_PATH" \
   -F "metadata=@$MAPPING_PATH")
 
-rm "$PODCAST_HTML_PATH"
+clean_up_html
 
 cat <<EOF > "$HEADER_FILE_PATH"
 <rss version="2.0">
-    <channel>
-        <title>$(echo "$PODCAST_JSON" | jq -r '.result.title')</title>
-        <link>$(echo "$PODCAST_JSON" | jq -r '.result.link')</link>
-        <language>ru</language>
-        <description>$(echo "$PODCAST_JSON" | jq -r '.result.description')</description>
-        <image>
-            <url>$(echo "$PODCAST_JSON" | jq -r '.result.image')</url>
-        </image>
-    </channel>
-</rss>
+  <channel>
+    <title>$(echo "$PODCAST_JSON" | jq -r '.result.rss.channel.title')</title>
+    <link>$(echo "$PODCAST_JSON" | jq -r '.result.rss.channel.link')</link>
+    <language>$(echo "$PODCAST_JSON" | jq -r '.result.rss.channel.language')</language>
+    <description>$(echo "$PODCAST_JSON" | jq -r '.result.rss.channel.description')</description>
+    <image>
+      <url>$(echo "$PODCAST_JSON" | jq -r '.result.rss.channel.image.url')</url>
+    </image>
+  </channel>
 EOF
 
 cp "$HEADER_FILE_PATH" "$RSS_FILE_PATH"
+echo "</rss>" >> $RSS_FILE_PATH
