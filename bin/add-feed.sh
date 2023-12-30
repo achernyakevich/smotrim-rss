368285855d0962131521d0649ebfd5d66d0fdf4f
Alex Chernyakevich <tch@scand.com>
2023-12-30 09:35:59 +0300

    Refactoring: variables renaming and groupping; introduction clean_up function (first version)

--- bin/add-feed.sh
+++ bin/add-feed.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+clean_up() {
+    echo "Clean up begins:"
+    rm -vrf "$PODCAST_FEED_DIR"
+    rm -v "$PODCAST_HTML_PATH"
+#    rm -rf "$PODCAST_FEED_DIR"
+#    rm -rf "$PODCAST_FEED_DIR"
+    echo "Clean up ends"
+}
+
 PODCAST_ID=$1
+SCRIPT_FLAG=$2
+
+MAPPING_PATH="./bin/mapping.json"
+FEEDS_BASE_DIR="./feeds"
+PODCAST_FEED_DIR="$FEEDS_BASE_DIR/$PODCAST_ID"
+HEADER_FILE_PATH="$PODCAST_FEED_DIR/header_$PODCAST_ID.xml"
+RSS_FILE_PATH="$PODCAST_FEED_DIR/rss_$PODCAST_ID.xml"
+PODCAST_HTML_PATH="./tmp/podcast-$PODCAST_ID.html"
+
 if [ -z "$PODCAST_ID" ]; then
     echo -e "Usage:\t./bin/add-feed.sh <podcast_id>"
     echo -e "\nAdd empty RSS feed for smotrim.ru podcast with <podcast_id>."
@@ -11,13 +29,6 @@ else
     fi
 fi
 
-
-MAPPING_PATH="./bin/mapping.json"
-FEEDS_BASE_DIR="./feeds"
-PODCAST_FEED_DIR="$FEEDS_BASE_DIR/$PODCAST_ID"
-HEADER_FILE_PATH="$PODCAST_FEED_DIR/header_$PODCAST_ID.xml"
-RSS_FILE_PATH="$PODCAST_FEED_DIR/rss_$PODCAST_ID.xml"
-
 if [ -d "$PODCAST_FEED_DIR" ]; then
     echo "Error: podcast's feed folder already exists ($PODCAST_FEED_DIR). Check podcast id or remove target RSS folder."
     exit 3
@@ -25,17 +36,16 @@ fi
 mkdir "$PODCAST_FEED_DIR"
 
 
-PODCAST_HTML_PATH="./tmp/podcast-$PODCAST_ID.html"
 RESPONSE=$(curl -s -w '%{http_code}' -o "$PODCAST_HTML_PATH" "https://smotrim.ru/podcast/$PODCAST_ID")
 STATUS=$(echo $RESPONSE | awk '{print $NF}')
 
 if [ "$STATUS" -ne 200 ]; then
-    echo "Error: can\`t download podcast with id $PODCAST_ID"
-    rm -rf "./$PODCAST_ID"
+    echo "Error: can't download podcast's HTML (URL: https://smotrim.ru/podcast/$PODCAST_ID, HTTP Status: $STATUS)"
+    clean_up
     exit 1
 fi
 
-API_RESPONSE=$(curl -s -X 'POST' \
+PODCAST_JSON=$(curl -s -X 'POST' \
   'https://api.d2d.work/document/process' \
   -H 'accept: application/json' \
   -H 'Content-Type: multipart/form-data' \
@@ -47,12 +57,12 @@ rm "$PODCAST_HTML_PATH"
 cat <<EOF > "$HEADER_FILE_PATH"
 <rss version="2.0">
     <channel>
-        <title>$(echo "$API_RESPONSE" | jq -r '.result.title')</title>
-        <link>$(echo "$API_RESPONSE" | jq -r '.result.link')</link>
+        <title>$(echo "$PODCAST_JSON" | jq -r '.result.title')</title>
+        <link>$(echo "$PODCAST_JSON" | jq -r '.result.link')</link>
         <language>ru</language>
-        <description>$(echo "$API_RESPONSE" | jq -r '.result.description')</description>
+        <description>$(echo "$PODCAST_JSON" | jq -r '.result.description')</description>
         <image>
-            <url>$(echo "$API_RESPONSE" | jq -r '.result.image')</url>
+            <url>$(echo "$PODCAST_JSON" | jq -r '.result.image')</url>
         </image>
     </channel>
 </rss>
