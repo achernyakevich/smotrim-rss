db4d2c31abf755d5ce5832b35bbd04bc75210ddd
Alex Chernyakevich <tch@scand.com>
2023-12-30 13:59:25 +0300

    Refactoring: JSON structure, JSON to XML transformation for header, simplified flow of creation and clean up, adding TODO for automated commit of added feed

--- bin/add-feed.sh
+++ bin/add-feed.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-clean_up_feed_dir() {
-    rm -vrf "$PODCAST_FEED_DIR"
-}
-clean_up_html() {
-    rm -v "$PODCAST_HTML_PATH"
-}
-
 PODCAST_ID=$1
-SCRIPT_FLAG=$2
+SCRIPT_FLAGS=$2
 
 MAPPING_PATH="./bin/mapping.json"
 FEEDS_BASE_DIR="./feeds"
 PODCAST_FEED_DIR="$FEEDS_BASE_DIR/$PODCAST_ID"
-HEADER_FILE_PATH="$PODCAST_FEED_DIR/rss_$PODCAST_ID.header"
-RSS_FILE_PATH="$PODCAST_FEED_DIR/rss_$PODCAST_ID.xml"
 PODCAST_HTML_PATH="./tmp/podcast-$PODCAST_ID.html"
+PODCAST_JSON_PATH="./tmp/podcast-$PODCAST_ID.json"
+PODCAST_HEADER_PATH="./tmp/rss-$PODCAST_ID.header"
+PODCAST_RSS_PATH="./tmp/rss-$PODCAST_ID.xml"
 
 if [ -z "$PODCAST_ID" ]; then
     echo -e "Usage:\t./bin/add-feed.sh <podcast_id>"
@@ -31,39 +25,57 @@ if [ -d "$PODCAST_FEED_DIR" ]; then
     echo "Error: podcast's feed folder already exists ($PODCAST_FEED_DIR). Check podcast id or remove target RSS folder."
     exit 3
 fi
-mkdir "$PODCAST_FEED_DIR"
 
 
 RESPONSE=$(curl -s -w '%{http_code}' -o "$PODCAST_HTML_PATH" "https://smotrim.ru/podcast/$PODCAST_ID")
 STATUS=$(echo $RESPONSE | awk '{print $NF}')
-
 if [ "$STATUS" -ne 200 ]; then
     echo "Error: can't download podcast's HTML (URL: https://smotrim.ru/podcast/$PODCAST_ID, HTTP Status: $STATUS)"
-    clean_up_feed_dir
     exit 1
 fi
 
-PODCAST_JSON=$(curl -s -X 'POST' \
-  'https://api.d2d.work/document/process' \
+curl -s -X 'POST' 'https://api.d2d.work/document/process' \
   -H 'accept: application/json' \
   -H 'Content-Type: multipart/form-data' \
   -F "document=@$PODCAST_HTML_PATH" \
-  -F "metadata=@$MAPPING_PATH")
+  -F "metadata=@$MAPPING_PATH" \
+| jq '{"channel": .result.channel}' > $PODCAST_JSON_PATH
 
-clean_up_html
+jq -r '
+  def json2xml(indent):
+    . as $in
+    | reduce to_entries[] as $el (
+        "";
+        . + "\n" + indent + "<\($el.key)>" +
+        (
+          if $in[$el.key] | type == "object" then
+            "\($in[$el.key] | json2xml(indent+"  "))\n" + indent
+          elif $in[$el.key] | type == "array" then
+            "\($in[$el.key][] | json2xml(indent+"  "))"
+          else
+            "\($in[$el.key])"
+          end
+        ) +
+        "</\($el.key)>"
+    );
+  "<rss version=\"2.0\">" +
+  (json2xml("  "))
+' "$PODCAST_JSON_PATH" > "$PODCAST_HEADER_PATH"
 
-cat <<EOF > "$HEADER_FILE_PATH"
-<rss version="2.0">
-  <channel>
-    <title>$(echo "$PODCAST_JSON" | jq -r '.result.rss.channel.title')</title>
-    <link>$(echo "$PODCAST_JSON" | jq -r '.result.rss.channel.link')</link>
-    <language>$(echo "$PODCAST_JSON" | jq -r '.result.rss.channel.language')</language>
-    <description>$(echo "$PODCAST_JSON" | jq -r '.result.rss.channel.description')</description>
-    <image>
-      <url>$(echo "$PODCAST_JSON" | jq -r '.result.rss.channel.image.url')</url>
-    </image>
-  </channel>
-EOF
+cp "$PODCAST_HEADER_PATH" "$PODCAST_RSS_PATH" && echo "</rss>" >> "$PODCAST_RSS_PATH"
 
-cp "$HEADER_FILE_PATH" "$RSS_FILE_PATH"
-echo "</rss>" >> $RSS_FILE_PATH
+# copying final version of header and RSS to the feed's folder
+mkdir "$PODCAST_FEED_DIR"
+cp "$PODCAST_HEADER_PATH" "$PODCAST_FEED_DIR"
+cp "$PODCAST_RSS_PATH" "$PODCAST_FEED_DIR"
+
+# Clean Up ./tmp folder
+if ! [[ $SCRIPT_FLAGS =~ "KEEPTMP" ]]; then
+    echo "Clean up for $PODCAST_ID:"
+    rm -vrf ./tmp/*$PODCAST_ID*
+fi
+
+# Add to VCS and commit feed's folder
+if [[ $SCRIPT_FLAGS =~ "COMMIT" ]]; then
+    echo TODO
+fi
